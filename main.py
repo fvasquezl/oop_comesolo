836f5c1dc5778c98f3ddab16028a5e4e8052dcc2
Faustino Vasquez <vasquez.limon@gmail.com>
2025-11-24 13:13:39 -0800

    creating Comesolo Class

--- main.py
+++ main.py
@@ -3,27 +3,42 @@
 class Board:
     def __init__(self, rows:int):
         self.rows = rows
-        self.board:list[str]= []
-        self.board_length = self.rows * (self.rows +1) // 2
-        self.create_board()
+        self.cells:list[str]= []
+        self.cells_length = self.rows * (self.rows +1) // 2
+        self.initialize_cells()
 
-    def create_board(self, default_value:str="1"):
-        self.board = [default_value] * self.board_length
+    def initialize_cells(self, default_value:str="1"):
+        self.cells = [default_value] * self.cells_length
 
-    def print_board(self, values: list[str], i: int) -> None:
-        row_str = [f"\033[33m{value}\033[0m" if value=="0" else value for value in values]
-        print("  " * (self.rows - i) + "   ".join(row_str))
-
-    def create_row(self):
+    def print_cells(self):
         start_pos = 0
         for i in range(self.rows):
             end_pos = start_pos + i + 1
-            values = self.board[start_pos:end_pos]
-            self.print_board(values, i)
+            values = self.cells[start_pos:end_pos]
+            row_str = [f"\033[33m{value}\033[0m" if value == "0" else value for value in values]
+            print("  " * (self.rows - i) + "   ".join(row_str))
             start_pos = end_pos
+
+class Comesolo:
+    def __init__(self, rows:int):
+        self.board =Board(rows)
+        self.create_board()
+
+    def create_board(self):
+        print("\n--- Estado inicial del tablero ---")
+        self.board.print_cells()
+
+
+    def make_move(self):
+        self.board.cells[1] = '0'
+        print(f"\n--- Movimiento en 1 ---")
+        self.board.print_cells()
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    board = Board(5)
-    board.create_row()
+    comesolo = Comesolo(5)
+    comesolo.make_move()
+
+
