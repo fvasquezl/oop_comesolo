fa9c43590605d6b77a16aa701f431605c9a47d7e
Faustino Vasquez <vasquez.limon@gmail.com>
2025-11-24 16:05:56 -0800

    addinf rules

--- main.py
+++ main.py
@@ -19,14 +19,48 @@ class Board:
             print("  " * (self.rows - i) + "   ".join(row_str))
             start_pos = end_pos
 
+
+class Rules:
+    def __init__(self, rows:int):
+        self.rows = rows
+        self.rules:dict[int, list[list[int]]] = {}
+        self.find_child()
+
+    def find_left_child(self,pos, row):
+        middle = pos + row + 1
+        end = middle + row + 2
+        self.rules.setdefault(pos,[]).append([middle,end])
+
+    def find_right_child(self,pos, row):
+        middle = pos + row + 2
+        end = middle + row + 3
+        self.rules.setdefault(pos,[]).append([middle,end])
+
+    def find_child(self):
+        start_pos = 0
+        for row in range(self.rows):
+            end_pos = start_pos + row + 1
+            array_range = range(start_pos, end_pos)
+            for pos in array_range:
+                self.find_left_child(pos, row)
+                self.find_right_child(pos,row)
+                # if row >=2:
+                #     self.find_brother(pos,row)
+            start_pos = end_pos
+
+        print(self.rules)
 class Comesolo:
     def __init__(self, rows:int):
-        self.board =Board(rows)
+        self.board=Board(rows)
         self.create_board()
+        self.rules=Rules(rows)
 
     def create_board(self):
         print("\n--- Estado inicial del tablero ---")
         self.board.print_cells()
+
+    def create_rules(self):
+        self.rules.find_child()
 
 
     def make_move(self):
@@ -36,9 +70,13 @@ class Comesolo:
 
 
 
+
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     comesolo = Comesolo(5)
-    comesolo.make_move()
+    #comesolo.make_move()
+
 
 
