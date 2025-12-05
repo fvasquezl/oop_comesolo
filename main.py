3d15fdabbb49dd62de955b8a9b9549346306b88a
Faustino Vasquez <vasquez.limon@gmail.com>
2025-12-04 16:01:58 -0800

    Rules dict

--- main.py
+++ main.py
@@ -19,41 +19,6 @@ class Board:
             print("  " * (self.rows - i) + "   ".join(row_str))
             start_pos = end_pos
 
-
-#           0
-#         1   2
-#       3   4   5
-#     6   7   8   9
-#   10  11  12  13  14
-
-#                                           P2+1     P3+1
-# [                      P0    P1     P2     P3       P4       P5
-#                            P0+i    P1+1   P2+i    P3+1      P4+1
-# 0 [0, 1, 2, 3, 4, 5]  [ 0, 0+1=1, 1+1=2, 2+1= 3,  3+1= 4,  4+1= 5]
-# 1 [1, 3, 4, 6, 7, 8]  [ 1, 1+2=3, 3+1=4, 4+2= 6,  6+1= 7,  7+1= 8]
-# 2 [2, 4, 5, 7, 8, 9]  [ 2, 2+2=4, 4+1=5, 5+2= 7,  7+1= 8,  8+1= 9]
-# 3 [3, 6, 7,10,11,12]  [ 3, 3+3=6, 6+1=7, 7+3=10, 10+1=11, 11+1=12]
-# 4 [4, 7, 8,11,12,13]  [ 4, 4+3=7, 7+1=8, 8+3=11, 11+1=12, 12+1=13]
-# 5 [5, 8, 9,12,13,14]  [ 5, 5+3=8, 8+1=9, 9+3=12, 12+1=13, 13+1=14]
-# ]
-
-# [
-# 0 [0, 1, 2, 3, 4, 5]
-#      [0,1,3]
-#      [0,2,5]
-#      [3,4,5]
-#
-# 1 [1, 3, 4, 6, 7, 8]
-# 2 [2, 4, 5, 7, 8, 9]
-# 3 [3, 6, 7,10,11,12]
-# 4 [4, 7, 8,11,12,13]
-# 5 [5, 8, 9,12,13,14]
-# ]
-
-
-
-
-
 class Rules:
     def __init__(self, rows:int):
         self.total_rows = rows
@@ -61,29 +26,48 @@ class Rules:
         self.max_rows = self.total_rows - self.triangle_depth + 1
         self.sub_triangles = self.find_sub_triangles()
 
-    def create_rules(self):
-        for row in self.sub_triangles:
-
-
 
     def find_sub_triangles(self):
-        triangles = []
+        all_rules_dict = {}
 
         for i in range(1, self.max_rows + 1):
             start_of_row = (i - 1) * i // 2
+
 
             for j in range(i):
                 P0 = start_of_row + j
                 P1 = P0 + i
                 P2 = P1 + 1
-                P3 = P2 + i
-                P4 = P3 + 1
+                P3 = P1 + i+ 1
+                P4 = P2 + i+ 1
                 P5 = P4 + 1
 
-                triangle = [P0, P1, P2, P3, P4, P5]
-                triangles.append(triangle)
-        print(triangles)
-        return triangles
+                lines = [
+                    # Vertical (P0 - P1 - P3)
+                    (P0, P1, P3), (P3, P1, P0),
+                    # Diagonal Derecha (P0 - P2 - P5)
+                    (P0, P2, P5), (P5, P2, P0),
+                    # Horizontal/Base (P3 - P4 - P5)
+                    (P3, P4, P5), (P5, P4, P3)
+                ]
+
+                for origen, eliminar, destino in lines:
+
+                    # El movimiento es [Pieza a Eliminar, Destino]
+                    movimiento = [eliminar, destino]
+
+                    # Si el origen ya está en el diccionario, agregar el nuevo movimiento
+                    if origen in all_rules_dict:
+                        # Solo agregamos el movimiento si no es un duplicado
+                        if movimiento not in all_rules_dict[origen]:
+                            all_rules_dict[origen].append(movimiento)
+                    else:
+                        # Si el origen es nuevo, creamos la entrada
+                        all_rules_dict[origen] = [movimiento]
+
+
+        print(all_rules_dict)
+        return lines
 
 
 
@@ -107,5 +91,40 @@ if __name__ == '__main__':
     comesolo = Comesolo(5)
     #comesolo.make_move()
 
+#            0
+#          1   2
+#        3   4   5
+#      6   7   8   9
+#    10  11  12  13  14
 
+
+#
+#                         P0   P1     P2      P3         P4        P5
+# [                          P0+i    P1+1   P1+i+1     P2+i+1     P4+1
+# 0 [0, 1, 2, 3, 4, 5]  [ 0, 0+1=1, 1+1=2, 1+1+1= 3,  2+1+1= 4,  4+1= 5]
+# 1 [1, 3, 4, 6, 7, 8]  [ 1, 1+2=3, 3+1=4, 3+2+1= 6,  4+2+1= 7,  7+1= 8]
+# 2 [2, 4, 5, 7, 8, 9]  [ 2, 2+2=4, 4+1=5, 4+2+1= 7,  5+2+1= 8,  8+1= 9]
+# 3 [3, 6, 7,10,11,12]  [ 3, 3+3=6, 6+1=7, 6+3+1=10,  7+3+1=11, 11+1=12]
+# 4 [4, 7, 8,11,12,13]  [ 4, 4+3=7, 7+1=8, 7+3+1=11,  8+3+1=12, 12+1=13]
+# 5 [5, 8, 9,12,13,14]  [ 5, 5+3=8, 8+1=9, 8+3+1=12,  9+2+1=13, 13+1=14]
+# ]
+
+#         P0-0
+#       P1-1  P2-2      P3 = P1+i+1
+#     P3-3  P4-4  P5-5    P4 = P2+i+1
+#
+#   [0, 1, 3], [0, 2, 5], [3, 4, 5]]
+
+
+# [
+# 0 [0, 1, 2, 3, 4, 5]
+#  0  1   3
+# [P0,P1,P3]
+# [
+
+#
+#  0 {[1,3],[2,5]}
+#  3 {[1,0],[4,5]}
+#  5 {[2,0],[4,3]}
+#
 
